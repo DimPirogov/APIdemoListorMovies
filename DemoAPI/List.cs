985a0f0ae8c4986e92444f4a273789dffb636b9e
Dimitri <dimpirogovp@hotmail.com>
2023-07-02 19:05:52 +0200

    Add project files.

---
+++ DemoAPI/List.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoAPI
+{
+    public class List
+    {
+        public int ListId { get; set; }
+        public DateTime Date { get; set; }
+        public string? ListName { get; set; }
+        public ICollection<Conversation>? Conversations { get; set; } = new List<Conversation>();
+    }
+}
